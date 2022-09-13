3b33ef4bab5218e62205b94d3a0822f58499244c
edwardbudaza <edward.budaza@yahoo.co.za>
2022-09-14 00:32:42 +0200

    "Cleaned the Header"

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="text-center bg-orange-800 text-white p-4 mb-10">
-    <div class="text-3xl md:text-5xl font-bold mb-3"><i class="fa fa-virus-covid"></i> Covid-19 Dashboardz </div>
+    <div class="text-3xl md:text-5xl font-bold mb-3"><i class="fa fa-virus-covid"></i> Covid-19 Dashboard </div>
     <p>API by <a class="text-orange-300" href="https://covid19api.com" target="_blank">covid19api.com</a></p>
   </header>
   
